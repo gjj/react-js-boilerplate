11a933e30282333e2b822a80c37e744ff12f67a4
Goi Jia Jian <jiajiannn@gmail.com>
2020-12-25 14:41:23 +0800

    feat: add Ant Design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,8 @@
 import React from "react";
-import logo from "../logo.svg";
+import { Button } from "antd";
 
 function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button type="primary">Button</Button>;
 }
 
 export default Home;
